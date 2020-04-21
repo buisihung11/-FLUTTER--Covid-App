98256c788a43ea44dfa89675502f6d0c3ff78242
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-21 16:32:12 +0700

    Modify constraint

--- lib/Constraint.dart
+++ lib/Constraint.dart
@@ -6,6 +6,7 @@ const kBackground = Color(0xFF473F97);
 const kPrimary = Color(0xFF4C79FF);
 const kSecondary = Color(0xFF999FBF);
 const kTextLightColor = Color(0xFFFFFFFF);
+const kTextSubtitleColor = kTextLightColor;
 const kTextDarkColor = Color(0xFF0D1333);
 const kPreventionBackground = Color(0xFFFFE5EE);
 
@@ -13,9 +14,12 @@ const kPreventionBackground = Color(0xFFFFE5EE);
 const kHeadingTextStyle = TextStyle(
   fontSize: 22,
   fontWeight: FontWeight.w600,
+  color: Colors.white,
 );
 
-const kSubTextStyle = TextStyle(fontSize: 16, color: kTextLightColor);
+const kSubTextStyle = TextStyle(
+  fontSize: 16,
+);
 
 const kTitleTextstyle = TextStyle(
   fontSize: 18,
