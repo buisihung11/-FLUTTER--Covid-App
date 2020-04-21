dc5a0e9cc8f25a743ff46d90371136fc5f264023
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-04-21 15:02:02 +0700

    Add color constrait

---
+++ lib/Constraint.dart
@@ -0,0 +1,24 @@
+// COLORS
+
+import 'package:flutter/material.dart';
+
+const kBackground = Color(0xFF473F97);
+const kPrimary = Color(0xFF4C79FF);
+const kSecondary = Color(0xFF999FBF);
+const kTextLightColor = Color(0xFFFFFFFF);
+const kTextDarkColor = Color(0xFF0D1333);
+const kPreventionBackground = Color(0xFFFFE5EE);
+
+// Text Style
+const kHeadingTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+const kSubTextStyle = TextStyle(fontSize: 16, color: kTextLightColor);
+
+const kTitleTextstyle = TextStyle(
+  fontSize: 18,
+  color: kTextDarkColor,
+  fontWeight: FontWeight.bold,
+);
